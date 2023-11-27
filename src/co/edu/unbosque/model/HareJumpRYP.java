9f9584fc9359ca94deb85bbb50503d37dcd02f8e
DevJohann <felipepoveda273@gmail.com>
2023-11-26 20:01:00 -0500

    Corregida la implementacion

--- src/co/edu/unbosque/model/HareJumpRYP.java
+++ src/co/edu/unbosque/model/HareJumpRYP.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 public class HareJumpRYP {
 
-    private int[][] jumps;
+	private int[][] jumps;
     private int[][] board;
     private int endX, endY;
     private int[][] moves = {{1, 2}, {-1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, 1}, {-2, -1}};
+    private ArrayList<int[]> takenSteps;
 
     public HareJumpRYP(int p, int q, int n, int m, int x0, int y0, int endX, int endY) {
         this.endX = endX;
@@ -34,29 +35,56 @@ public class HareJumpRYP {
     }
 
     private boolean hop(int x, int y) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> (heuristic(a[0], a[1]))));
-        pq.add(new int[]{x, y});
+    	PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> (heuristic(a[0], a[1]))));
+    	pq.add(new int[]{x, y});
+        takenSteps = new ArrayList<>();
         int step = 1;
 
         while (!pq.isEmpty()) {
-            int[] current = pq.poll();
+        	int[] current = pq.poll();
             x = current[0];
             y = current[1];
+            takenSteps.add(new int[]{x, y});
             board[x][y] = step++;
-            
-            
+
             if (x == endX && y == endY) {
                 return true;
             }
+
+            boolean foundNextMove = false;
+            int nextX = -1, nextY = -1;
 
             for (int[] move : moves) {
                 int nx = x + move[0];
                 int ny = y + move[1];
 
-                if (isValidMove(nx, ny)) {
-                    pq.add(new int[]{nx, ny});
+                if (isValidMove(nx, ny) && board[nx][ny] == 0) {
+                	pq.add(new int[] {nx, ny});
+                    foundNextMove = true;
+                    nextX = nx;
+                    nextY = ny;
+                    break;
                 }
             }
+
+            if (!foundNextMove) {
+                // Backtrack to previous step
+                if (takenSteps.size() <= 1) {
+                    return false; // No more available moves to backtrack
+                }
+
+                // Rama y poda: Verificar si hay un camino más corto desde este punto
+                int[] lastStep = takenSteps.remove(takenSteps.size() - 1);
+                x = lastStep[0];
+                y = lastStep[1];
+
+                if (step < board[x][y] || board[x][y] == 0) {
+                	System.out.println("Retrocediendo...");
+                    board[x][y] = step; // Actualizar el valor en el tablero con el número de pasos actual
+                } else {
+                    continue; // Evitar explorar esta rama si hay un camino más corto desde este punto
+                }
+            } 
         }
         return false;
     }
@@ -71,6 +99,10 @@ public class HareJumpRYP {
                 System.out.print(cell + " ");
             }
             System.out.println();
+        }
+        System.out.println("Pasos: ");
+        for(int[] i : takenSteps) {
+        	System.out.println(i[0] + ", " + i[1]);
         }
     }
 
